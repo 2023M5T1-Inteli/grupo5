80cb55e11792a9155a188ef2477861857c33c511
André Luís Lessa Junior <99192411+andlljr@users.noreply.github.com>
2023-03-15 16:56:07 -0300

    Adicionar testes para criação do mapa

--- src/front-end/maverick/src/main.tsx
+++ src/front-end/maverick/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './routes/router';
+import Test from './test/Test'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <Router/>
+    <Test/>
 )
