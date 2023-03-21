757c0ed1d5a8717fe7dd4987f36a362d0421acfc
André Luís Lessa Junior <99192411+andlljr@users.noreply.github.com>
2023-03-21 14:05:03 -0300

    Ajuste na main

--- src/front-end/maverick/src/main.tsx
+++ src/front-end/maverick/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './routes/router';
-import Test from './test/Test'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
